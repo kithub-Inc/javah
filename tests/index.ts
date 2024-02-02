fe3aac766ea148caeabfa9891fa18246275fdd7e
ICe1BotMaker <ice1github@gmail.com>
2024-02-02 16:42:47 +0900

    [Test] Javah `^12.7.0b` beta test

--- tests/index.ts
+++ tests/index.ts
@@ -1,31 +1,36 @@
-import FrameWork from '../src';
+import FrameWork, { Request } from '../src';
 const Javah = new FrameWork();
 
 @Javah.Create()
 export default class App {
-    @Javah.Service(`/products`)
+    @Javah.Service(`/api/products`) @Javah.Cors()
     public Products = class extends Javah.Page {
-        public constructor() {
-            super(); const { request } = this;
-            
-            if (request.method === `get`) this.get();
+        public request: Request;
+
+        public constructor(request: Request) {
+            super(); this.request = request;
+            if (this.request.method === `get`) this.get();
             else this.post();
         }
 
         public get() {
-            const { response, send, usePath } = this;
-
-            response.status = 200;
-            response.header = { 'Content-type': 'application/json' };
-            
-            send(usePath(`tests/products.json`));
+            this.response.header = { 'Content-type': 'text/html charset=utf-8' };
+            this.response.send(`<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <p>이건 겟인데 ㅠㅠ</p>
+            </body>
+            </html>`);
         }
 
         public post() {
-            const { request, response, useError } = this;
-
-            response.status = 404;
-            useError(`${request.url} ${response.status} Not Found`);
+            this.response.header = { 'Content-type': 'application/json' };
+            this.response.send(this.usePath(`tests/products.json`));
         }
     }
 }
